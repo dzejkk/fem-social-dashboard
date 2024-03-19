0630732124ac6de4a3454c4b73f93290f01fd4ba
dzejk <kontrajakub2@gmail.com>
2024-03-19 18:55:25 +0000

    Relocating of project

---
+++ app.js
@@ -0,0 +1,35 @@
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+        document.getElementById('slider').checked = false;
+    } else {
+        setTheme('theme-light');
+      document.getElementById('slider').checked = true;
+    }
+})();
+
+
+
+const a = 5;
+const b = 6; 
+
+
+const x = a + b;
+
+console.log(x);
